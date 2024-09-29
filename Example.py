9294da4b97f004de0d70746b370da4b831e5c393
WokyDoky <daniel.benavent3@gmail.com>
2024-09-28 22:46:22 -0600

    This is using the Manhattan distance instead of the euclidean distance. Perfomance is worse.
    It is taking too many iteration to find a solution.
    
    Next branch will hopefully fix this.
    
    In this branch, it kinda works but not for every map. Will ask professor how to solve.

--- Example.py
+++ Example.py
@@ -48,96 +48,59 @@ def return_path(current_node):
     :return:
     """
 def astar(maze, start, end, allow_diagonal_movement=False):
-
-    # Create start and end node
     start_node = Node(None, start)
     start_node.g = start_node.h = start_node.f = 0
     end_node = Node(None, end)
     end_node.g = end_node.h = end_node.f = 0
 
-    # Initialize both open and closed list
     open_list = []
-    closed_list = []
+    closed_list = set()
+    open_dict = {start_node.position: start_node}
 
-    # Heapify the open_list and Add the start node
     heapq.heapify(open_list)
     heapq.heappush(open_list, start_node)
 
-    # Adding a stop condition
-    outer_iterations = 0
-    max_iterations = (len(maze[0]) * len(maze) // 2)
-
-    # what squares do we search
-    adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
+    adjacent_squares = [(0, -1), (0, 1), (-1, 0), (1, 0)]
     if allow_diagonal_movement:
-        adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1),)
+        adjacent_squares += [(-1, -1), (-1, 1), (1, -1), (1, 1)]
 
-    # Loop until you find the end
-    while len(open_list) > 0:
-        outer_iterations += 1
-
-        if outer_iterations > max_iterations:
-            # if we hit this point return the path such as it is
-            # it will not contain the destination
-            warn("giving up on pathfinding too many iterations")
-            return return_path(current_node)
-
-            # Get the current node
+    while open_list:
         current_node = heapq.heappop(open_list)
-        closed_list.append(current_node)
+        closed_list.add(current_node.position)
 
-        # Found the goal
         if current_node == end_node:
             return return_path(current_node)
 
-        # Generate children
         children = []
 
-        for new_position in adjacent_squares:  # Adjacent squares
-
-            # Get node position
+        for new_position in adjacent_squares:
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (
-                    len(maze[len(maze) - 1]) - 1) or node_position[1] < 0:
+            if (node_position[0] < 0 or node_position[0] >= len(maze) or
+                node_position[1] < 0 or node_position[1] >= len(maze[0])):
                 continue
 
-            # Make sure walkable terrain
             terrain_cost = maze[node_position[0]][node_position[1]]
-
             if terrain_cost == 0:
                 continue
 
-            # Create new node
             new_node = Node(current_node, node_position)
-
-            # Append
             children.append(new_node)
 
-        # Loop through children
         for child in children:
-            # Child is on the closed list
-            if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
+            if child.position in closed_list:
                 continue
 
-            # Create the f, g, and h values
-            child.g = current_node.g + terrain_cost
-            ## Manhattan distance
-            #  Note: It does work but it is worse than using the Euclidean distance.
-            #   Quite a lot worse.
-            # abs(new_node.position[0] - end_node.position[0]) + abs(new_node.position[1] - end_node.position[1])
-            child.h = np.sqrt((child.position[0] - end_node.position[0]) ** 2 + (child.position[1] - end_node.position[1]) ** 2)
-
+            child.g = current_node.g + maze[child.position[0]][child.position[1]]
+            child.h = np.sqrt((child.position[0] - end_node.position[0]) ** 2 +
+                              (child.position[1] - end_node.position[1]) ** 2)
             child.f = child.g + child.h
 
-            # Child is already in the open list
-            if len([open_node for open_node in open_list if
-                    child.position == open_node.position and child.g > open_node.g]) > 0:
+            if child.position in open_dict and child.g >= open_dict[child.position].g:
                 continue
 
-            # Add the child to the open list
             heapq.heappush(open_list, child)
+            open_dict[child.position] = child
 
     warn("Couldn't get a path to destination")
     return None
@@ -146,7 +109,7 @@ def astar(maze, start, end, allow_diagonal_movement=False):
 
 def main():
 
-    mazeOption = 1
+    mazeOption = 3
     maze = Utilities.maze_chooser(mazeOption)
 
     start = Utilities.start_aligner(mazeOption)
