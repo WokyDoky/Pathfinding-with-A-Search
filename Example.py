d486dad56e9a397445e6673844fbf836a8044644
WokyDoky <daniel.benavent3@gmail.com>
2024-10-01 22:34:31 -0600

    Working.

--- Example.py
+++ Example.py
@@ -15,17 +15,11 @@ choose diagonal.
 
 """
 
-#Changes
-
-
-from warnings import warn
 import random
 import heapq
 import numpy as np
 import timeit
-
-import Utilities
-
+import matplotlib.pyplot as plt
 
 class Node:
     """
@@ -132,36 +126,192 @@ def astar(maze, start, end, heuristic, allow_diagonal_movement = False):
     return None
 
 
+def print_info(start, end, maze, path, print_extra = False):
+
+    print("Path cost: ", calculate_path_cost(maze, path))
+    print("Manhattan distance: ", calculate_manhattan_distance(start, end))
+    print(path)
+    if print_extra:
+        print(np.matrix(maze))
+        path_printer(maze, path, start, end)
+
+def path_printer(maze, path, start, end):
+    if path is None: return "No Solution"
+    print(" ", end=" ")
+    for row in range(len(maze[0])): print(row, end = " ")
+    print("")
+    for i in range(len(maze)):
+        print(i, end = " ")
+        for j in range(len(maze[i])):
+            if (i, j) == start:
+                print("@", end = " ")
+                continue
+            if (i, j) == end:
+                print("%", end = " ")
+                continue
+            elif (i, j) in path:
+                print("X", end=" ")
+            else:
+                print("O", end=" ")
+        print()  # Print a newline after each row
+
+def start_aligner (i):
+    if i == 1:
+        return 1,2
+    elif i == 2:
+        return 3,6
+    elif i == 3:
+        return 1,2
+    elif i == 4:
+        return 1,1
+    elif i == 5:
+        return 2,1
+    else: return 2,4
+def end_aligner (i):
+    if i == 1:
+        return 4,3
+    elif i == 2:
+        return 5,1
+    elif i == 3:
+        return 8,8
+    elif i == 4:
+        return 1,7
+    elif i == 5:
+        return 3,7
+    else: return 7,6
+def maze_chooser (i):
+    if i == 1:
+        maze = [[2,4,2,1,4,5,2],
+                [0,1,2,3,5,3,1],
+                [2,0,4,4,1,2,4],
+                [2,5,5,3,2,0,1],
+                [4,3,3,2,1,0,1]]
+        return maze
+    elif i == 2:
+        maze = [[1,3,2,5,1,4,3],
+                [2,1,3,1,3,2,5],
+                [3,0,5,0,1,2,2],
+                [5,3,2,1,5,0,3],
+                [2,4,1,0,0,2,0],
+                [4,0,2,1,5,3,4],
+                [1,5,1,0,2,4,1]]
+        return maze
+    elif i == 3:
+        maze = [[2,0,2,0,2,0,0,2,2,0],
+                [1,2,3,5,2,1,2,5,1,2],
+                [2,0,2,2,1,2,1,2,4,2],
+                [2,0,1,0,1,1,1,0,0,1],
+                [1,1,0,0,5,0,3,2,2,2],
+                [2,2,2,2,1,0,1,2,1,0],
+                [1,0,2,1,3,1,4,3,0,1],
+                [2,0,5,1,5,2,1,2,4,1],
+                [1,2,2,2,0,2,0,1,1,0],
+                [5,1,2,1,1,1,2,0,1,2]]
+        return maze
+    elif i == 4:
+        maze = [[0, 0, 2, 0, 2, 0, 0, 2, 2, 0],
+                [0, 1, 1, 1, 1, 1, 2, 5, 1, 2],
+                [0, 0, 1, 2, 1, 2, 1, 2, 4, 2],
+                [0, 0, 1, 0, 1, 1, 1, 0, 0, 1],
+                [1, 0, 0, 0, 1, 0, 1, 2, 2, 2],
+                [2, 0, 2, 2, 1, 0, 1, 2, 1, 0],
+                [1, 0, 2, 1, 1, 1, 4, 3, 0, 1],
+                [2, 1, 1, 1, 1, 2, 1, 2, 4, 1],
+                [1, 2, 2, 2, 0, 2, 0, 1, 1, 0],
+                [5, 1, 2, 1, 1, 1, 2, 0, 1, 2]]
+        return maze
+    elif i == 5:
+        maze = [[1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+                [5, 1, 2, 3, 4, 5, 1, 2, 3, 4],
+                [4, 5, 1, 2, 3, 4, 5, 1, 2, 3],
+                [3, 4, 5, 1, 2, 3, 4, 5, 1, 2],
+                [2, 3, 4, 5, 1, 2, 3, 4, 5, 1],
+                [1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+                [5, 1, 2, 3, 4, 5, 1, 2, 3, 4],
+                [4, 5, 1, 2, 3, 4, 5, 1, 2, 3],
+                [3, 4, 5, 1, 2, 3, 4, 5, 1, 2],
+                [2, 3, 4, 5, 1, 2, 3, 4, 5, 1]]
+        return maze
+    else:
+        maze = [[1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+        return maze
+
+
+def calculate_path_cost(maze, path):
+    if path is None: return "No solution found"
+    total_cost = 0
+    for position in path[1:]:  #Ignore the starting position cost
+        x, y = position
+        total_cost += maze[x][y]
+    return total_cost
+
+def calculate_manhattan_distance(star, end):
+    return abs(end[0] - star[0]) + abs(end[1] - star[1])
+
+
 def timeing_different_heuristic_formulas(mazeOption):
     print(f"Maze: {mazeOption}")
-    maze = Utilities.maze_chooser(mazeOption)
+    maze = maze_chooser(mazeOption)
 
-    start = Utilities.start_aligner(mazeOption)
-    end = Utilities.end_aligner(mazeOption)
-    combinations = [(False, False), (False, True), (True, False), (True, True)]
+    start = start_aligner(mazeOption)
+    end = end_aligner(mazeOption)
+
+    times = []
 
     for integer_value in range(4):  # 0 to 3
         start_time = timeit.default_timer()
         astar(maze, start, end, integer_value)  # Call your function here
         elapsed = timeit.default_timer() - start_time
+        times.append(elapsed)
         print(
             f"Execution time for h({integer_value}) (Start: {start}, End: {end}): {elapsed:.10f} seconds")
 
-def print_info(start, end, maze, path, print_extra = False):
+    return times
 
-    print("Path cost: ", Utilities.calculate_path_cost(maze, path))
-    print("Manhattan distance: ", Utilities.calculate_manhattan_distance(start, end))
-    print(path)
-    if print_extra:
-        print(np.matrix(maze))
-        Utilities.path_printer(maze, path, start, end)
+def make_graph():
+    heuristic_times = {i: [] for i in range(4)}  # Store times for each heuristic
+
+    # Run the function for all maps
+    for maze_option in range(1, 6):  # Assuming maps are numbered from 1 to 5
+        times = timeing_different_heuristic_formulas(maze_option)
+        for i, time in enumerate(times):
+            heuristic_times[i].append(time)
+
+    # Calculate average times for each heuristic function
+    average_times = [sum(heuristic_times[i]) / len(heuristic_times[i]) for i in range(4)]
+
+    # Plotting the graph
+    plt.bar(range(4), average_times, color=['blue', 'green', 'red', 'orange'])
+    plt.xlabel('Heuristic Function')
+    plt.ylabel('Average Execution Time (seconds)')
+    plt.title('Average Execution Time for Heuristic Functions Across All Maps')
+    plt.xticks(range(4), [f'h({i})' for i in range(4)])
+    plt.show()
+
+
+"""
+=====================================================================================
+===================================== MAIN ==========================================
+=====================================================================================
+"""
 
 def main():
     #Do not use unless you want to generate graph
     #   Utilities.make_graph()
     """
-    This will have to be deleted
-    Needs to prompt the user what map they want and what heuristic.
+    Code might look weird since it was originally two files with a utility class.
+    Had to merge them for this assignment.
+    If the original code wants to be seen follow this link:
+    https://github.com/WokyDoky/Pathfinding-with-A-Search/blob/addingMoreHeuristicValues/Utilities.py
     """
     print("Enter 2 values, first value will choose a map, second value will choose a heuristic.")
     print("Values for the map range from 1 - 5 and for the heuristic value from 1 - 4.")
@@ -170,10 +320,10 @@ def main():
     value1, value2 = map(int, input("Separate values by a space > ").split())
 
     maze_option = value1
-    maze = Utilities.maze_chooser(maze_option)
+    maze = maze_chooser(maze_option)
 
-    start = Utilities.start_aligner(maze_option)
-    end = Utilities.end_aligner(maze_option)
+    start = start_aligner(maze_option)
+    end = end_aligner(maze_option)
 
     path = astar(maze, start, end, value2)
 
